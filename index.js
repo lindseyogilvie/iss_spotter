0d675a7bab8d8604b93cc906930aceda6953de7a
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-20 22:24:56 -0800

    Refactored ISS monitoring app using promises instead of nested callbacks

--- index.js
+++ index.js
@@ -39,15 +39,7 @@
 
 // Flyover Times for my Location Callback
 const { nextISSTimesForMyLocation } = require('./iss');
-
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const { printPassTimes } = require('./print-pass-times');
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
@@ -56,3 +48,4 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   printPassTimes(passTimes);
 });
+
---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIp = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipwho.is/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const location = {
+    latitude: JSON.parse(body).latitude,
+    longitude: JSON.parse(body).longitude,
+  };
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${location.latitude}&lon=${location.longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIp)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+
+module.exports = {nextISSTimesForMyLocation};
